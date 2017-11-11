3caef2c905f4441cc6457aad5420cb803007cf1a
Jerika Ostler <jerika.ostler@gmail.com>
2017-11-11 15:21:09 -0700

    dropdown information

--- MissionSite/MissionSite/Controllers/SelectedMissionController.cs
+++ MissionSite/MissionSite/Controllers/SelectedMissionController.cs
@@ -11,7 +11,7 @@ namespace MissionSite.Controllers
         public ActionResult Mission()
         {
             List<SelectListItem> missions = new List<SelectListItem>();
-            missions.Add(new SelectListItem { Text = "Select a mission", Value = "0" });
+            missions.Add(new SelectListItem { Text = "Select a mission", Value = "0", Disabled = true, Selected = true });
             missions.Add(new SelectListItem { Text = "Philippines, Cavite", Value = "1" });
             missions.Add(new SelectListItem { Text = "Africa, Mozambique Maputo", Value = "2" });
             missions.Add(new SelectListItem { Text = "Mexico, Mexico City Southeast", Value = "3" });
@@ -21,12 +21,36 @@ namespace MissionSite.Controllers
 
         public ViewResult MissionChosen(string MissionName)
         {
-            if (MissionName.Equals("0"))
-                ViewBag.messageString = "Colorado";
+            if (MissionName.Equals("1"))
+            {
+                ViewBag.NameOfMission = "Philippines, Cavite";
+                ViewBag.PresidentName = "President Aaron B. Speirs";
+                ViewBag.MissionAddress = "Avenida St. Pag-asa Subdivision \nMolino, Bacoor City \n4102 Cavite Philippines";
+                ViewBag.Language = "Tagalog";
+                ViewBag.Climate = "Tropical Rainforest";
+                ViewBag.DomReligion = "Catholic";
+                ViewBag.Flag = "";
+            }
             else if (MissionName.Equals("1"))
-                ViewBag.messageString = "Texas";
+            {
+                ViewBag.NameOfMission = "Africa, Mozambique Maputo";
+                ViewBag.PresidentName = "President Joni Koch";
+                ViewBag.MissionAddress = "Caixa postal 1166 Correio \nCentral Maputo, Maputo, Mozambique";
+                ViewBag.Language = "Portuguese";
+                ViewBag.Climate = "Tropical";
+                ViewBag.DomReligion = "Catholic";
+                ViewBag.Flag = "";
+            }
             else if (MissionName.Equals("2"))
-                ViewBag.messageString = "Utah";
+            {
+                ViewBag.NameOfMission = "Mexico, Mexico City Southeast";
+                ViewBag.PresidentName = "President Lindsay K. Mann";
+                ViewBag.MissionAddress = "Av. del Taller 540 \nCol. Jardin Balbuna del Venustiano Carranza \nCiudad de Mexico, Mexico  15900";
+                ViewBag.Language = "Spanish";
+                ViewBag.Climate = "Subtropical";
+                ViewBag.DomReligion = "Catholic";
+                ViewBag.Flag = "";
+            }
 
             return View("FAQ");
         }
