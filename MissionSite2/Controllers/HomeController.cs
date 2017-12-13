1cc2a79f34a1cf3914ad7dee991b15739d6042d0
jp3ckham <32501145+jp3ckham@users.noreply.github.com>
2017-12-12 19:21:52 -0700

    Updated Everything

---
+++ MissionSite2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MissionSite.Controllers
+{
+    public class HomeController : Controller
+    {   
+        //calls the index view
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //calls the about view
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hello";
+
+            return View();
+        }
+
+        //calls the contact page
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MissionSite2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MissionSite2.Startup))]
+namespace MissionSite2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
