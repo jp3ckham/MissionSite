66f4b3aecc4c035c8a6f002980bdbb6f34761e6f
Melanie Mitsvotai <melanie.mitsvotai@gmail.com>
2017-12-13 19:47:21 -0700

    ViewBags getting from model, passing int parameter

--- MissionSite2/Controllers/SelectedMissionController.cs
+++ MissionSite2/Controllers/SelectedMissionController.cs
@@ -4,56 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MissionSite2.Models;
 
 namespace MissionSite.Controllers
 {
     public class SelectedMissionController : Controller
     {
+        //Create Database context variable
         public MissionSite2Context db = new MissionSite2Context();
 
+        //Put missions in a list for the dropdown list in the view
         public ActionResult Mission()
         { 
             ViewBag.Missions = db.Mission.ToList();
             return View();
         }
 
-        public ViewResult MissionChosen(string MissionName)
-        {
-            //this is the logic after you click dropdown this fills the viewbag for the page
-            if (MissionName.Equals("1"))
-            {   
-                //philippines
-                ViewBag.NameOfMission = "Philippines, Cavite";
-                ViewBag.PresidentName = "President Aaron B. Speirs";
-                ViewBag.MissionAddress = "Avenida St. Pag-asa Subdivision \nMolino, Bacoor City \n4102 Cavite Philippines";
-                ViewBag.Language = "Tagalog";
-                ViewBag.Climate = "Tropical Rainforest";
-                ViewBag.DomReligion = "Catholic";
-                ViewBag.Flag = "philflag.png";
-            }
-            else if (MissionName.Equals("2"))
-            {
-                //Mozambique
-                ViewBag.NameOfMission = "Africa, Mozambique Maputo";
-                ViewBag.PresidentName = "President Joni Koch";
-                ViewBag.MissionAddress = "Caixa postal 1166 Correio \nCentral Maputo, Maputo, Mozambique";
-                ViewBag.Language = "Portuguese";
-                ViewBag.Climate = "Tropical";
-                ViewBag.DomReligion = "Catholic";
-                ViewBag.Flag = "Mozflag.png";
-            }
-            else if (MissionName.Equals("3"))
-            {
-                //Mexico
-                ViewBag.NameOfMission = "Mexico, Mexico City Southeast";
-                ViewBag.PresidentName = "President Lindsay K. Mann";
-                ViewBag.MissionAddress = "Av. del Taller 540 \nCol. Jardin Balbuna del Venustiano Carranza \nCiudad de Mexico, Mexico  15900";
-                ViewBag.Language = "Spanish";
-                ViewBag.Climate = "Subtropical";
-                ViewBag.DomReligion = "Catholic";
-                ViewBag.Flag = "mexflag.jpg";
-            }
-
+        public ViewResult MissionChosen(int id)
+        {   
+            //Lookup the missionID and return the data for that mission
+            ViewBag.NameOfMission = db.Mission.Find(id).MissionName;
+            ViewBag.PresidentName = db.Mission.Find(id).MissionPresidentName;
+            ViewBag.MissionAddress = db.Mission.Find(id).MissionAddress;
+            ViewBag.Language = db.Mission.Find(id).MissionLanguage;
+            ViewBag.Climate = db.Mission.Find(id).MissionClimate;
+            ViewBag.DomReligion = db.Mission.Find(id).DominantReligion;
+            ViewBag.Flag = db.Mission.Find(id).MissionFlag;
 
             return View("FAQ");
         }
